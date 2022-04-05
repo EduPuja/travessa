3dda92e220544e354def206ffad5c0cdd820c5cf
EduPuja <edpufa@inspalamos.cat>
2022-04-05 17:19:41 +0000

    menu per escollir si vols anar a admin o a usuari

---
+++ user/menu.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Interficies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" href="../assets/img/icons/footway.ico"/>
+
+    
+</head>
+    <body>
+        <!--Aqui se entra nomes si ets admin , sino t'envas directament al index.php-->
+        <h1> OPCIONS DE MENU</h1>
+
+        <section class="vh-100 gradient-custom">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                <div class="card-body p-5 text-center">
+      
+                  <div class="mb-md-5 mt-md-4 pb-5">
+      
+                    <h2 class="fw-bold mb-2 text-uppercase">OPCIO MENU</h2>
+                    <p class="text-white-50 mb-5">A quina interface vols anar Menu admin  o el otro </p>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+        
+    </body>
+</html>
