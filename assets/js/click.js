aa6a30476f1c5e2eb88281295bbc7e8958ea46a2
EduPuja <edpufa@inspalamos.cat>
2022-04-05 18:04:55 +0000

    canvi index

--- assets/js/click.js
+++ assets/js/click.js
@@ -1,38 +1,4 @@
-
-//general
-function enviarMySql()
-{
-  location.href ="mysql/home.html";   //TE ENVIA AL HOME.HMTL
-}
-function enviarPostgrest()
-{
-  location.href ="postgres/home.html";   //TE ENVIA AL HOME.HMTL
-}
-
-//mysql
-function enviarLoginMySql() 
-{
-  location.href="mysql/login";
-}
-function enviarRegisterMySql()
-{
-    location.href="mysql/register";
-}
 function enviarHomeMySql()
 {
-  location.href ="mysql/home.html";
-}
-
-//postgres
-function enviarLoginPostgres() 
-{
-  location.href="postgres/login";
-}
-function enviarRegisterPostgres()
-{
-    location.href="postgres/register";
-}
-function enviarHomePostgres()
-{
-  location.href ="postgres/home.html";
+  alert("hola");
 }
