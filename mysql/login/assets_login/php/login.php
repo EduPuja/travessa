60bbf041faf9c23121e38f4c2f456cf1f1ee646f
EdPuFa <edpufa@inspalamos.cat>
2022-04-14 20:15:13 +0200

    login changes

--- mysql/login/assets_login/php/login.php
+++ mysql/login/assets_login/php/login.php
@@ -1,46 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="sweetalert2.all.min.js"></script>
+
+  <title>LOGIN php</title>
+</head>
+
+</html>
 <?php
-include "../../../assets/php/connexioBD.php";
+#include "../../../assets/php/connexioBD.php";
+require("../../../../assets/php/connexioBD.php");
 
 $correu = $_POST["email"];
 $password = $_POST["contrassenya"];
 
 $consulta = "SELECT isAdmin,email,contrassenya from usuari WHERE email = '$correu'";
+
 $query = mysqli_query($connexio, $consulta);
+// ens dona el un array 
 $rows = mysqli_fetch_assoc($query);
-//print_r($rows);
+
 
 if($rows >= 0)
 {
-  if($row["isAdmin"] == 1)
+  if($rows['isAdmin'] == 1)
   {
-    echo "hola";
+    
+    
     if(password_verify($password, $rows["contrassenya"]))
     {
+      echo "hola ADMIN";
      // header("Location: /mysql/user/menu.php");
-      echo"location.href ='../../user/menu.php'";
+      #echo"location.href ='../../user/menu.php'";
       //redireccio a pagina admin
     }
-    else{
-      echo "La contrassenya no es correcte";
+    else
+    {
+      // poner alert
+      #echo "La contrassenya no es correcte";
+      #echo "Contrass"
     }
+    # window.location.href="../../../login"
   }
   else
   {
+    
+    
     if(password_verify($password, $rows["contrassenya"]))
     {
+      echo "HOLA USUARI";
       header("Location: ../../user/assets_user/menuUser.php");
-      /*echo"
-      <script>
-        location.href ='../../user/menuUser.php'
-      </script>";*/
-      //redireccio a pagina user
+    
     }
-    else{
-      echo "La contrassenya no es correcte";
+    else
+    {
+      // poner alert
+      echo "La contrassenya  no es correcte";
     }
+    
   }
  
 }
-else{
+else
+{
+  // poner alert
   echo "El correu no es correcte";
 }
