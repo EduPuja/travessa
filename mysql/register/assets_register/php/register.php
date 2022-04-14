6b856752eea318d2003c9124ffb20415049ad71c
EdPuFa <edpufa@inspalamos.cat>
2022-04-14 21:19:16 +0200

    register

--- mysql/register/assets_register/php/register.php
+++ mysql/register/assets_register/php/register.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="../../assets/img/icons/footway.ico"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="../../../../assets/css/main.css" rel="stylesheet" type="text/css">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="icon" type="image/png" href="../../../../assets/img/icons/footway.ico"/>
+
+  <title>Register</title>
+</head>
+    <body>
+      
+    <section class="vh-100 gradient-custom">
+        
+      </section>
+    </body>
+</html>
+
 <?php
 #include "../../../../assets/php/connexioBD.php";
 #REQUIERE EL CONEXIO PHP   
@@ -28,11 +51,26 @@ $hash = password_hash($password, PASSWORD_DEFAULT);
         $exito = mysqli_query($connexio,$insert);
         if($exito)
         {
-            echo "INSERTADA";
+             #echo "INSERTADA";
 
 
                 // REDIRECIO CAP A LOGIN
-                header("Location: ../../../login");
+            #header("Location: ../../../login");
+
+            echo '<script>
+            
+              Swal.fire({
+                icon: "success",
+                title: "OKA",
+                text: "Registre Correcte" ,
+                confirmButtonText:
+                "<i class=fa fa-thumbs-up>GO</i> " +
+                "<a href=../../../login>Log in</a> ",
+              })
+    
+            
+              </script>';
+          
         }
         /*else
         {
