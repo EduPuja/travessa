d8107d88d5e0a432426f1c95b7dd32c5c7ba5d06
EdPuFa <edpufa@inspalamos.cat>
2022-04-14 21:14:45 +0200

    THE FINAL LOGIN

--- mysql/login/assets_login/php/login.php
+++ mysql/login/assets_login/php/login.php
@@ -8,10 +8,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="../../../../assets/css/main.css" rel="stylesheet" type="text/css">
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="icon" type="image/png" href="../../../../assets/img/icons/footway.ico"/>
+
   <title>Login</title>
 </head>
-<!--AQUI POT HABERI UN BODY -->
+    <body>
+      
+    <section class="vh-100 gradient-custom">
+        
+      </section>
+    </body>
 </html>
 <?php
 
