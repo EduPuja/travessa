720eac0295c318e8fa9a549816d243eb7f5662ba
EdPuFa <edpufa@inspalamos.cat>
2022-04-16 19:52:13 +0200

    canvis home and posibles canvis index.php

--- mysql/user/index.php
+++ mysql/user/index.php
@@ -20,13 +20,14 @@ if(!isset($_SESSION['usuari']))
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="icon" type="image/png" href="../../assets/img/icons/footway.ico">
-  <!--EL STYLES LO HE PUESTO AQUI  -->
+  <!--CSS DE TAULES   -->
   <link href="../assets_user/css/user.css" type="text/css" rel="stylesheet" />
+  <link href="../../assets/css/main.css" rel="stylesheet" type="text/css">
   
 
 <script async src='/cdn-cgi/bm/cv/669835187/api.js'></script>
 </head>
-	<body>
+	<body class="gradient-custom ">
 		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 			<div class="container-fluid">
 				<form class="container-fluid justify-content-start">
