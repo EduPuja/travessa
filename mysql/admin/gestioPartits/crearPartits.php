31f4f0a5943f2f1983728d98c849dfcb9205f5b6
EdPuFa <edpufa@inspalamos.cat>
2022-04-16 22:00:41 +0200

    favicon

--- mysql/admin/gestioPartits/crearPartits.php
+++ mysql/admin/gestioPartits/crearPartits.php
@@ -23,7 +23,7 @@ if(!isset($_SESSION['usuari']))
     <title>Menu Interficies</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="icon" type="image/png" href="../../assets/img/icons/footway.ico"/>
+    <link rel="icon" type="image/png" href="../../../assets/img/icons/footway.ico"/>
     <link href="../../../../assets/css/main.css" rel="stylesheet" type="text/css">
     
 </head>
@@ -45,6 +45,7 @@ if(!isset($_SESSION['usuari']))
 						            <div class="form-outline form-white mb-4">
                                         <h5 class="text-white-50 mb-2">Equip Local</h5>
                                         <select class="form-control" name="equipLocal">
+                                        <optgroup label="Equips">
                                             <?php           
                                                 require("../../../assets/php/connexioBD.php");  
                                                 $query = "SELECT nom from equip";
@@ -55,6 +56,7 @@ if(!isset($_SESSION['usuari']))
                                                      echo "<option value='equip'>".$row['nom']."</option>";
                                                 }
                                             ?>
+                                            </optgroup>
                                         </select>
                                     </div>
 					            	<div class="form-outline form-white mb-4">
