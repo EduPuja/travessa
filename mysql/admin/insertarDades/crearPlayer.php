794b794a6a7c062a297a94ccc6a26dc4e32ffdc1
EdPuFa <edpufa@inspalamos.cat>
2022-04-18 19:55:39 +0200

    crearPlayer.php Update

--- mysql/admin/insertarDades/crearPlayer.php
+++ mysql/admin/insertarDades/crearPlayer.php
@@ -23,7 +23,34 @@ if(!isset($_SESSION['usuariAdmin']))
 </head>
 <body class="vh-100 gradient-custom">
     <!--Comentario de index admin-->
-  
+    <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                <div class="card-body p-5 text-center">
+      
+                      <div class="mb-md-5 mt-md-4 pb-5">
+          
+                        <h2 class="fw-bold mb-2 text-uppercase">Crear Jugador</h2>
+                        <p class="text-white-50 mb-5">Entra un jugador</p>
+                        <form class="from-control" action="###" method="post">
+                            <div class="form-outline form-white mb-4">
+                                <input type="text" name="nom" id="nom" class="form-control form-control-lg" placeholder="Nom Jugador" required/>
+                            </div>
+              
+                            <div class="form-outline form-white mb-4">
+                              <input type="number" name="dorsal" id="dorsal" class="form-control form-control-lg" placeholder="Num Dorsal" required/>
+                            </div>
+                            <button class="btn btn-outline-light btn-lg px-5" type="submit">Crear</button> 
+                        </form>
+                      </div>
+                      
+      
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
    
     
 </body>
