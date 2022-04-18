9b3d1cd70cfef394b8e9af72844d1800ffdbdba3
EdPuFa <edpufa@inspalamos.cat>
2022-04-18 22:08:09 +0200

    php de contacte

--- assets/php/contacte.php
+++ assets/php/contacte.php
@@ -6,5 +6,19 @@ $email = $_POST['email'];
 $descripcio =$_POST['descripcio'];
 
 
+$insert = "INSERT INTO contacte (nom,email,descripcio) VALUES($nom,$email,$descripcio)";
+
+$result = mysqli_query($connexio,$insert);
+
+if($result)
+{
+    echo "hola";
+}
+else
+{
+    echo "not good";
+}
+
+
 mysqli_close($connexio);
 ?>
