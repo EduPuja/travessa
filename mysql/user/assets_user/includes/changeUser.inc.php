62cb78a6f6f85b3f7954088e6c7e5b1d53b07df7
EdPuFa <edpufa@inspalamos.cat>
2022-04-19 11:33:38 +0200

    canvis

--- mysql/user/assets_user/includes/changeUser.inc.php
+++ mysql/user/assets_user/includes/changeUser.inc.php
@@ -1,8 +1,15 @@
 <?php
     require("../../../../assets/php/connexioBD.php");
 
+    $correu = $_POST['email'];
     $nom = $_POST['nom'];
     $cognom = $_POST['cognom'];
     $password = $_POST['contrassenya'];
+
+
+    $consulta = "SELECT isAdmin,email,contrassenya,nom from usuari WHERE email = '$correu'";
+    $query = mysqli_query($connexio, $consulta);
+    // ens dona el un array 
+    $rows = mysqli_fetch_assoc($query);
 
 ?>
