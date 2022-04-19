72443c085a7888fe01e31ca9b4503d13f7aecb93
EdPuFa <edpufa@inspalamos.cat>
2022-04-19 13:37:50 +0200

    PERFIL

--- mysql/user/perfil.php
+++ mysql/user/perfil.php
@@ -75,7 +75,8 @@ if(!isset($_SESSION['usuari']) && !isset($_SESSION['usuariAdmin']))
                                         #$query = "SELECT email,CASE WHEN isAdmin = 0 AND nom ='$_SESSION[usuari]' THEN nom WHEN isAdmin =1 AND nom='$_SESSION[usuariAdmin]' THEN nom END as nom_usuari,cognom,adreca,cartera FROM usuari";
 										#$nom="SELECT nom FROM usuari";
                                         #$query = "SELECT email,CASE WHEN isAdmin = 0 AND nom ='$_SESSION[usuari]' THEN nom WHEN isAdmin =1 AND nom='$_SESSION[usuariAdmin]' THEN nom END as nom_usuari,cognom,adreca,cartera FROM usuari";
-                                        $novaConsulta ="SELECT email,nom,cognom,adreca,cartera, FROM usuari WHERE email ='$_SESSION[usuariAdmin]'";
+                                       
+                                        $novaConsulta ="SELECT email,nom,cognom,adreca,cartera FROM usuari WHERE email ='$_SESSION[usuariAdmin]'";
                                         $result = mysqli_query($connexio,$novaConsulta);
 
                                         if($result)
