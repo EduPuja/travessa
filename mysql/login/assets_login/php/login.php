25a8ce7cfd97d6ef370d227bc827ed86f24f04c4
EdPuFa <edpufa@inspalamos.cat>
2022-04-19 14:55:19 +0200

    login pta madre

--- mysql/login/assets_login/php/login.php
+++ mysql/login/assets_login/php/login.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="../../assets/img/icons/footway.ico"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="../../../../assets/css/main.css" rel="stylesheet" type="text/css">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="icon" type="image/png" href="../../../../assets/img/icons/footway.ico"/>
+
+  <title>Login</title>
+</head>
+    <body class="vh-100 gradient-custom">
+      
+    
+    </body>
+</html>
 <?php
 session_start();      // SESION START PER DESPRES PODER COMPROVAR SI LA SESSIO ES D'UN ADMIN O DE UN USUARI
 require("../../../../assets/php/connexioBD.php");
@@ -21,80 +41,67 @@ else
     $query = mysqli_query($connexio, $consulta);
     // ens dona el un array 
     $rows = mysqli_fetch_assoc($query);
-    
-   
-    
-    
-    if($rows >= 0)
+
+     
+    if($rows['isAdmin'] == 1)
     {
-        // si ets admin
-        if($rows['isAdmin'] == 1)
-        {
-         
-            $_SESSION['usuariAdmin']= $rows['email'];     // creo una varialbe de sessio amb el correu d'admin
-            if(password_verify($password, $rows["contrassenya"])) 
-            { 
-              // COMPROVO QUE LA SI ESTA BE
 
-              if(isset($_SESSION['usuariAdmin']))
-              {
-                header("Location: ../../../admin/");
-              }
-              
-             
-               
-
-            }
-            else
-            {
-              
-              // password admin incorrecte
-                  
-                header("Location: ../../../login");
-                session_destroy();
-            }
+         // si ets admin
       
-        }
-        // SINO SI ES USUARI
-        else if($rows['isAdmin'] == 0)
+   
+      #echo "ets admin";
+      if(password_verify($password,$rows['contrassenya']))
+      {
+        $_SESSION['usuariAdmin'] = $rows['email'];
+        #echo "password admin oka";
+
+        if(isset($_SESSION['usuariAdmin']))
         {
-            $_SESSION['usuari']= $rows['email'];
-              if(password_verify($password, $rows["contrassenya"]))
-              {
-              
-                  // USUARI
-                  if(isset($_SESSION['usuari']))
-                  {
-                    header("Location: ../../../user/");
-                  }
+          #echo "SESSIO ADMINISTRADOR PTA MADRE";
+          //sessio administrador good
+         
 
-                 
-                 
-                
-              }
-              else
-              {
-                // fora de aqui si no esta be la contrassenya
-                header("Location: ../../../login");
-                session_destroy();
-                
-              }
-          
+          header("Location: /mysql/admin/");
         }
-
         else
         {
-          header("Location: ../../../login");
-          session_destroy();
+          #echo"sessio admin shit";
+          session_stop($_SESSION['usuariAdmin']);
+          header("Location: /mysql/login/");
         }
+      }
+      else
+      {
+        header("Location: /mysql/login/");
+         
+      }
+
       
     }
+
     else
     {
+       // ets USUARI
+       if(password_verify($password,$rows['contrassenya']))
+       {
+          $_SESSION['usuari'] = $rows['email'];
+
+          if(isset($_SESSION['usuari']))
+          {
+            header("Location: /mysql/user/");
+          }
+          else
+          {
+            
+            session_stop($_SESSION['usuari']);
+            header("Location: /mysql/login/");
+          }
+
+       }
      
-      header("Location: ../../../login");
-      session_destroy();
-   
     }
+    
+ 
+   
 }
 ?>
