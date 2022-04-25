1a02504280b9644fbfefb4540dc2308f81c12a15
EdPuFa <edpufa@inspalamos.cat>
2022-04-25 21:14:21 +0200

    title

--- mysql/admin/gestioPartits/index.php
+++ mysql/admin/gestioPartits/index.php
@@ -5,6 +5,7 @@ if(!isset($_SESSION['usuariAdmin']))
   echo '<script> 
   alert("No ets admin! FORA DE AQUI NO ET VOLEM  ");
   window.location.href="../login/";  </script>' ; 
+  session_destroy($_SESSION['userAdmin']);
 }
 
 
@@ -16,7 +17,7 @@ if(!isset($_SESSION['usuariAdmin']))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu Interficies</title>
+    <title>Gestio Partits</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="icon" type="image/png" href="../../../assets/img/icons/footway.ico"/>
