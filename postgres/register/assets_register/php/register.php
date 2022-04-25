d67c5c6fa6d2f223d36509e67a062fbe0b7f9f92
EdPuFa <edpufa@inspalamos.cat>
2022-04-25 17:48:23 +0200

    canvios postgres

--- postgres/register/assets_register/php/register.php
+++ postgres/register/assets_register/php/register.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/png" href="../../assets/img/icons/footway.ico"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link href="../../../../assets/css/main.css" rel="stylesheet" type="text/css">
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="icon" type="image/png" href="../../../../assets/img/icons/footway.ico"/>
-
-  <title>Register</title>
-</head>
-    <body class="vh-100 gradient-custom">
-      
-    
-    </body>
-</html>
-
 <?php
 #include "../../../../assets/php/connexioBD.php";
 #REQUIERE EL CONEXIO PHP   
-require("../../../../assets/php/connexioBD.php");
+require("../../../../assets/php/connexioPsql.php");
 
 // VARIALBES POST DEL HTML
 $nom = $_POST["nom"];
