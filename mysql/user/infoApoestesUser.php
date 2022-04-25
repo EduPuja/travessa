8207ab8fb1ac1b4b9b60d5e9a4cb25ef0282e471
EdPuFa <edpufa@inspalamos.cat>
2022-04-25 18:54:21 +0200

    title

--- mysql/user/infoApoestesUser.php
+++ mysql/user/infoApoestesUser.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION['usuari']))
 ?>
 <html lang="es">
 <head>
-  <title>Perfil</title>
+  <title>Apostes Usuari</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
